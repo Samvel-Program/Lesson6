58c7eefe8868b103edd8204dc44af2e62500cd72
Samvel Akopyan <samvel.mgsu@mail.ru>
2019-05-21 23:10:58 +0300

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created by OUT-Akopyan-SR on 16.05.2019.
+ */
+public class Program {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int nam = in.nextInt();
+        int mun = in.nextInt();
+        int sum = num + nam + mun / 3;
+        int summa = sum / 2;
+        if (
+                summa > 3
+        )
+            System.out.println("Программа выполнена корректно");
+
+
+    }
+}
